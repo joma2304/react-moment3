318d414c3a4531c64cd04028b2c866f5605b3907
Johan Magnusson <joma2304@student.miun.se>
2025-02-25 15:04:01 +0100

    Knapp från enskild produkt till alla produlter tillagd

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,5 +1,5 @@
 import "./ProductPage.css";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, NavLink } from "react-router-dom";
 import { useProducts } from "../context/ProductContext";
 import { useAuth } from "../context/AuthContext";
 import { useState, useEffect } from "react";
@@ -70,50 +70,53 @@ const ProductPage = () => {
   };
 
   return (
-    <div className="product-detail">
-      {editing ? (
-        <>
-          <h1>Redigera Produkt</h1>
-          <label htmlFor="name">Produktnamn</label>
-          <input type="text" name="name" value={updatedProduct?.name || ""} onChange={handleChange} />
+    <>
+    <NavLink to={"/"} className="backToStart">Gå till alla produkter</NavLink>
+      <div className="product-detail">
+        {editing ? (
+          <>
+            <h1>Redigera Produkt</h1>
+            <label htmlFor="name">Produktnamn</label>
+            <input type="text" name="name" value={updatedProduct?.name || ""} onChange={handleChange} />
 
-          <label htmlFor="description">Beskrivning</label>
-          <textarea name="description" value={updatedProduct?.description || ""} onChange={handleChange} />
+            <label htmlFor="description">Beskrivning</label>
+            <textarea name="description" value={updatedProduct?.description || ""} onChange={handleChange} />
 
-          <label htmlFor="brand">Märke</label>
-          <input type="text" name="brand" value={updatedProduct?.brand || ""} onChange={handleChange} />
+            <label htmlFor="brand">Märke</label>
+            <input type="text" name="brand" value={updatedProduct?.brand || ""} onChange={handleChange} />
 
-          <label htmlFor="price">Pris</label>
-          <input type="number" name="price" value={updatedProduct?.price || "0"} onChange={handleChange} />
+            <label htmlFor="price">Pris</label>
+            <input type="number" name="price" value={updatedProduct?.price || "0"} onChange={handleChange} />
 
-          <label htmlFor="amount">Antal i lager</label>
-          <input type="number" name="amount" value={updatedProduct?.amount || "0"} onChange={handleChange} />
-          <button onClick={handleSave}>Spara</button>
-          <button onClick={() => setEditing(false)}>Avbryt</button>
-        </>
-      ) : (
-        <>
-          <h1>{product.name}</h1>
-          <p><strong>Beskrivning:</strong> {product.description}</p>
-          <p><strong>Märke:</strong> {product.brand}</p>
-          <p><strong>Antal i lager:</strong> {product.amount} st</p>
-          <p className="price"><strong>Pris:</strong> {product.price} kr</p>
-          {user && (
-            <>
-              <button onClick={() => setEditing(true)}>Redigera</button>
-              <button onClick={handleDelete} className="delete-btn">Ta bort</button>
-            </>
-          )}
-        </>
-      )}
-      {showModal && (
-        <Modal
-          message={modalMessage}
-          onClose={() => setShowModal(false)}
-          onConfirm={modalMessage.includes("ta bort") ? confirmDelete : undefined}
-        />
-      )}
-    </div>
+            <label htmlFor="amount">Antal i lager</label>
+            <input type="number" name="amount" value={updatedProduct?.amount || "0"} onChange={handleChange} />
+            <button onClick={handleSave}>Spara</button>
+            <button onClick={() => setEditing(false)}>Avbryt</button>
+          </>
+        ) : (
+          <>
+            <h1>{product.name}</h1>
+            <p><strong>Beskrivning:</strong> {product.description}</p>
+            <p><strong>Märke:</strong> {product.brand}</p>
+            <p><strong>Antal i lager:</strong> {product.amount} st</p>
+            <p className="price"><strong>Pris:</strong> {product.price} kr</p>
+            {user && (
+              <>
+                <button onClick={() => setEditing(true)}>Redigera</button>
+                <button onClick={handleDelete} className="delete-btn">Ta bort</button>
+              </>
+            )}
+          </>
+        )}
+        {showModal && (
+          <Modal
+            message={modalMessage}
+            onClose={() => setShowModal(false)}
+            onConfirm={modalMessage.includes("ta bort") ? confirmDelete : undefined}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
